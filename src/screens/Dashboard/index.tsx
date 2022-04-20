c797378dde4ba7e3777763bef5f92d0d8cab8f02
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-20 18:26:05 -0300

    added Header and styles in Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  UserWrapper
+  } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+              <Photo  source={{ uri: 'https://avatars.githubusercontent.com/u/47507696?v=4'}}/>
+              <User>
+                <UserGreeting>Olá,</UserGreeting>
+                <UserName>Luciano</UserName>
+              </User>
+            </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
