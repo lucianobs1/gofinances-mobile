221d6dd0dd4b4a16937d2b8660710b1f87a4c746
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-20 19:34:57 -0300

    updated Header in Dashboard

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { Feather } from '@expo/vector-icons';
+import { Feather, createIconSet } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
@@ -53,7 +53,6 @@ export const UserName = styled.Text`
   font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `; 
-
 
 export const Icon = styled<any>(Feather)`
   color: ${({ theme }) => theme.colors.secondary };
