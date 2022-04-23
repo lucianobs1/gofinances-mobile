f1440f94d62d50bedb4061b64bad9c8f653b62f5
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-23 07:48:38 -0300

    added component HighlightCard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HIghlightCard";
+import { TransactionCard } from "../../components/TransactionCard";
 
 import {
   Container,
@@ -11,7 +12,9 @@ import {
   UserName,
   UserWrapper,
   Icon,
-  HighlightCards
+  HighlightCards,
+  Transactions,
+  Title,
   } from './styles';
 
 export function Dashboard() {
@@ -51,6 +54,13 @@ export function Dashboard() {
           lastTransaction="01 á 16 de abril"
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>
+          Listagem
+        </Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   )
 }
