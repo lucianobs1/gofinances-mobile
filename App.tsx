a2d13fdce7d720e545061c4c58a724631c779fb1
Luciano Brandao <lucianotetsu@gmail.com>
2022-04-24 22:28:34 -0300

    added styles in register screen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
-import { 
+import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
@@ -11,7 +11,8 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+// import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard  />
+      <Register  />
     </ThemeProvider>
   );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Button } from '../../components/Form/Button';
 import { Input } from '../../components/Form/Input';
 import {
   Container,
   Header,
   Title,
-  Form
+  Form,
+  Fields
  } from './styles';
 
 export function Register(){
@@ -14,12 +16,15 @@ export function Register(){
         <Title>Cadastro</Title>
       </Header>
       <Form>
-        <Input
-          placeholder='Nome'
-        />
-        <Input
-          placeholder='Preço'
-        />
+        <Fields>
+          <Input
+            placeholder='Nome'
+          />
+          <Input
+            placeholder='Preço'
+          />
+        </Fields>
+        <Button title="Enviar"/>
       </Form>
     </Container>
   )
